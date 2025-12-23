736ec758d9b29a219a16f7872a2530a76e384dd4
mano-dinnatcreusier <mano.dinnatcreusier@gmail.com>
2025-12-23 22:16:53 +0100

    changements de texte

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Correction IMPORTANTE :
-  // Pour un repo nommé "username.github.io", le site est à la racine.
-  // On met donc '/' au lieu du nom du repo.
+
   base: '/', 
 })
